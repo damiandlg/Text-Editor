3a90348f75e197508bdda2df9ed18de19355aca4
damiandlg <damian.delagarzaf@gmail.com>
2022-11-14 22:43:43 -0600

    deploying application to see if everything works

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// TODO: Implement asset cachin
 registerRoute(
   ({request}) => request.destination === 'image',
   new CacheFirst({

5a5730dc06d7e5e6f8fdcaf8e760e2b94b39f35b
damiandlg <damian.delagarzaf@gmail.com>
2022-11-14 22:30:34 -0600

    modified index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,6 +2,8 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
+import "./header";
+import "./install";
 
 const main = document.querySelector('#main');
 main.innerHTML = '';

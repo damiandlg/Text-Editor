5bef869bf07e59fa62c226d49ea5dbe72f880da2
damiandlg <damian.delagarzaf@gmail.com>
2022-11-14 22:03:51 -0600

    fixed DB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,7 +23,7 @@ export const putDb = async (content) =>{
 };
 
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 	const tx = jateDb.transaction('jate', 'readonly');
